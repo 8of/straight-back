7bc7f521636025419d971ac2b68e50bd5d241dd8
Andrei Konstantinov <8ofproject@gmail.com>
2021-07-25 17:43:17 +0300

    Added ads

--- mobile/src/main/java/org/of8/straightback/MainActivity.kt
+++ mobile/src/main/java/org/of8/straightback/MainActivity.kt
@@ -5,17 +5,28 @@ import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class MainActivity : AppCompatActivity() {
 
     private val mMainButton: FloatingActionButton
         get() = findViewById<FloatingActionButton>(R.id.startButton)
 
+    private lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.elevation = 0F
         setContentView(R.layout.activity_main)
         update(button = mMainButton, started = isServiceRunning())
+
+        // Ads
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 
     fun onButtonClick(view: View) {
